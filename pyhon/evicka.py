4a420c2a104b2fa105c66d54bad77468c8bd5e03
LukasDrsman <lukaskodr@gmail.com>
2019-10-19 08:43:59 +0200

    Added python code

---
+++ pyhon/evicka.py
@@ -0,0 +1,85 @@
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, GyroSensor
+from pybricks.parameters import Port, Stop, Direction, Button
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+left = Motor(Port.B)
+right = Motor(Port.C)
+sLeft = Motor(Port.D)
+sRight = Motor(Port.A)
+gyro = GyroSensor(Port.S1)
+gyro.reset_angle(0)
+robot = DriveBase(left, right, 43, 180)
+watch = StopWatch()
+
+def reset():
+    left.reset_angle(0)
+    right.reset_angle(0)
+    sLeft.reset_angle(0)
+    sRight.reset_angle(0)
+    gyro.reset_angle(0)
+
+def mov(spd=80, tm=1):
+    watch.resume()
+    watch.reset()
+    while(1):
+        robot.drive(spd, gyro.angle() * -1)
+        if(watch.time() >= tm*1000):
+            break
+    robot.stop()
+    watch.pause()
+
+def rot(spd=40, ang=0):
+    a = 1
+    if(ang > 0):
+        while(a == 1):
+            if(gyro.angle == ang):
+                left.stop(Stop.HOLD)
+                right.stop(Stop.HOLD)
+                gyro.reset_angle(0)
+                a = 0
+            left.run(spd)
+            right.run(spd * -1)
+            if(gyro.angle() > ang):
+                while(1):
+                    right.run(spd)
+                    left.run(spd * -1)
+                    if(gyro.angle() == ang):
+                        right.stop(Stop.HOLD)
+                        left.stop(Stop.HOLD)
+                        gyro.reset_angle(0)
+                        a = 0
+                    if(gyro.angle() < ang):
+                        break
+                
+    if(ang < 0):
+        while(a == 1):
+            if(gyro.angle == ang):
+                left.stop(Stop.HOLD)
+                right.stop(Stop.HOLD)
+                gyro.reset_angle(0)
+                a = 0
+            left.run(spd * -1)
+            right.run(spd)
+            if(gyro.angle() < ang):
+                while(1):
+                    right.run(spd * -1)
+                    left.run(spd)
+                    if(gyro.angle() == ang):
+                        right.stop(Stop.HOLD)
+                        left.stop(Stop.HOLD)
+                        gyro.reset_angle(0)
+                        a = 0
+                    if(gyro.angle() > ang):
+                        break
+                        
+def stick(spd=200, ang=0):
+    sLeft.run_target(spd, ang * -1, Stop.BRAKE)
+    sLeft.stop()
+
+def jaw(spd=200, ang=0):
+    sRight.run_target(spd, ang, Stop.BRAKE)
+    sLeft.run_target(spd, ang * -1, Stop.BRAKE)
+    sRight.stop()
+    sLeft.stop()
---
+++ pyhon/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env pybricks-micropython
+
+from evicka import *
+
+global switch
+switch = 0
+
+brick.sound.beep(1500, 500, 100)
+
+while(1):
+    if(Button.UP in brick.buttons()):
+        switch = 1
+    if(Button.LEFT in brick.buttons()):
+        switch = 2
+    if(Button.DOWN in brick.buttons()):
+        switch = 3
+    if(Button.RIGHT in brick.buttons()):
+        switch = 4
+    
+    if(switch == 1):
+        reset()
+        rot(40, -16)
+        mov(140, 9)
+        rot(30, -6)
+        mov(125, 4.4)
+        mov(-80, 1)
+        rot(40, 15)
+        mov(-80, 0.8)
+        rot(40, 20)
+        mov(-80, 0.7)
+        rot(40, 40)
+
+    if(switch == 2):
+        reset()
+        pass
+    if(switch == 3):
+        reset()
+        pass
+    if(switch == 4):
+        reset()
+        pass
+    switch = 0
